2ed4fc93b97f63b1f432d3fe9b1df3cbfffec64c
James Larson <timothy_james@tuta.io>
2026-05-23 12:32:49 -0700

    fix(leg_raise/leg_press): data correction

--- lib/data/sources/exercises/leg_raise.dart
+++ lib/data/sources/exercises/leg_raise.dart
@@ -2,115 +2,97 @@ const Map<String, dynamic> legRaise = {
   "pk": "cbbdaae8-7600-456f-b025-12d2050b3765",
   "name": "Leg Raise",
   "muscleGroups": ["Core"],
-  "primaryMuscles": ["rectus abdominis", "wrist flexors", "wrist extensors"],
+  "primaryMuscles": ["rectus abdominis", "iliopsoas"],
   "secondaryMuscles": ["external obliques"],
   "variations": {
     "hanging": {
       "excludedVariations": [
-        "flutter kick",
-        "knee-raise",
         "lying",
-        "single-leg",
-        "toes-to-bar",
+        "captain's chair",
+        "bench",
+        "incline bench"
       ],
     },
     "lying": {
       "excludedVariations": [
-        "flutter kick",
         "hanging",
-        "knee-raise",
-        "single-leg",
-        "toes-to-bar",
+        "captain's chair",
+        "bench",
+        "incline bench",
+        "toes-to-bar"
       ],
     },
     "single-leg": {
       "excludedVariations": [
-        "flutter kick",
-        "hanging",
-        "knee-raise",
-        "lying",
-        "toes-to-bar",
+        "flutter kick"
       ],
     },
     "knee-raise": {
       "excludedVariations": [
-        "flutter kick",
-        "hanging",
-        "lying",
-        "single-leg",
         "toes-to-bar",
+        "flutter kick"
       ],
     },
     "toes-to-bar": {
       "excludedVariations": [
-        "flutter kick",
-        "hanging",
-        "knee-raise",
         "lying",
-        "single-leg",
+        "captain's chair",
+        "bench",
+        "incline bench",
+        "knee-raise",
+        "flutter kick"
       ],
     },
     "flutter kick": {
       "excludedVariations": [
-        "hanging",
-        "knee-raise",
-        "lying",
         "single-leg",
         "toes-to-bar",
+        "knee-raise"
       ],
     },
     "weighted": {
       "excludedVariations": [
         "band",
-        "bench",
-        "cable",
-        "captain's chair",
-        "incline bench",
+        "cable"
       ],
     },
     "captain's chair": {
       "excludedVariations": [
-        "band",
+        "hanging",
+        "lying",
         "bench",
-        "cable",
         "incline bench",
-        "weighted",
+        "toes-to-bar"
       ],
     },
     "bench": {
       "excludedVariations": [
-        "band",
-        "cable",
+        "hanging",
+        "lying",
         "captain's chair",
         "incline bench",
-        "weighted",
+        "toes-to-bar"
       ],
     },
     "incline bench": {
       "excludedVariations": [
-        "band",
-        "bench",
-        "cable",
+        "hanging",
+        "lying",
         "captain's chair",
-        "weighted",
+        "bench",
+        "toes-to-bar"
       ],
     },
     "band": {
       "excludedVariations": [
-        "bench",
-        "cable",
-        "captain's chair",
-        "incline bench",
         "weighted",
+        "cable"
       ],
     },
     "cable": {
       "excludedVariations": [
-        "band",
-        "bench",
-        "captain's chair",
-        "incline bench",
         "weighted",
+        "band"
       ],
     },
   },
