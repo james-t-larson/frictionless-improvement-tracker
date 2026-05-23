30ed29fa29936f2bde13745cc7cbe60c9f84f9a4
James Larson <timothy_james@tuta.io>
2026-05-22 23:29:48 -0700

    fix(y_raise/data-correction): add y raise config

---
+++ lib/data/sources/exercises/y_raise.dart
@@ -0,0 +1,18 @@
+const Map<String, dynamic> yRaise = {
+  "pk": "1ad6e60e-b467-4a02-8f2e-d9a8c857b655",
+  "name": "Y Raise",
+  "muscleGroups": ["Shoulders"],
+  "primaryMuscles": ["trapezius", "posterior deltoid"],
+  "secondaryMuscles": ["serratus anterior", "rhomboids", "supraspinatus"],
+  "variations": {
+    "cable": {
+      "excludedVariations": [],
+    },
+    "dumbbell": {
+      "excludedVariations": [],
+    },
+    "band": {
+      "excludedVariations": [],
+    },
+  },
+};
