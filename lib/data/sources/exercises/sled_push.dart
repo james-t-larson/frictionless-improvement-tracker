d3c055e663da2b1abc0d5de243887df633faae25
James Larson <timothy_james@tuta.io>
2026-05-24 11:14:08 -0700

    fix(sled_push): data correct

--- lib/data/sources/exercises/sled_push.dart
+++ lib/data/sources/exercises/sled_push.dart
@@ -5,67 +5,38 @@ const Map<String, dynamic> sledPush = {
   "primaryMuscles": ["gluteus maximus", "quadriceps femoris"],
   "secondaryMuscles": ["hamstrings", "gastrocnemius", "anterior deltoid"],
   "variations": {
-    "loaded": {
-      "excludedVariations": [
-        "backward",
-        "heavy",
-        "high-handles",
-        "lateral",
-        "low-handles",
-        "single-arm",
-        "sprint",
-      ],
-    },
     "sprint": {
       "excludedVariations": [
         "backward",
         "heavy",
-        "high-handles",
         "lateral",
-        "loaded",
-        "low-handles",
         "single-arm",
       ],
     },
     "low-handles": {
       "excludedVariations": [
         "backward",
-        "heavy",
         "high-handles",
         "lateral",
-        "loaded",
-        "single-arm",
-        "sprint",
       ],
     },
     "high-handles": {
       "excludedVariations": [
         "backward",
-        "heavy",
-        "lateral",
-        "loaded",
         "low-handles",
-        "single-arm",
-        "sprint",
       ],
     },
     "single-arm": {
       "excludedVariations": [
         "backward",
         "heavy",
-        "high-handles",
         "lateral",
-        "loaded",
-        "low-handles",
         "sprint",
       ],
     },
     "backward": {
       "excludedVariations": [
-        "heavy",
         "high-handles",
-        "lateral",
-        "loaded",
         "low-handles",
         "single-arm",
         "sprint",
@@ -73,31 +44,16 @@ const Map<String, dynamic> sledPush = {
     },
     "heavy": {
       "excludedVariations": [
-        "backward",
-        "high-handles",
-        "lateral",
-        "loaded",
-        "low-handles",
         "single-arm",
         "sprint",
       ],
     },
     "lateral": {
       "excludedVariations": [
-        "backward",
-        "heavy",
-        "high-handles",
-        "loaded",
         "low-handles",
         "single-arm",
         "sprint",
       ],
-    },
-    "prowler": {
-      "excludedVariations": ["banded"],
-    },
-    "banded": {
-      "excludedVariations": ["prowler"],
     },
   },
 };
